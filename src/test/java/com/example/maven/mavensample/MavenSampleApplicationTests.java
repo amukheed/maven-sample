74c9e59977fdab3958f2f31ec77f7ddca60081cb
Abdul Mukheed <Abdul.Mukheed@Nwea.org>
2023-11-02 23:48:38 -0700

    Initial Commit

---
+++ src/test/java/com/example/maven/mavensample/MavenSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maven.mavensample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
